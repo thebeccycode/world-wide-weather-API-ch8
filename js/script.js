d24b4835d317e6ad6974268c64a8d6bf34f26e25
Rebecca Sellers <rlsellers@hotmail.co.uk>
2023-01-25 16:17:00 +0000

    minor changes, notes added to js file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//add my own API key
+
+let APIkey = "3426654f99a0dfa7522e4aab3cb64af2"
+
+
+//create a weather dashboard with form inputs, this must link to the html
+
+
+//when a user views the weather in a city they must be able to see the city name, the data, an icon representation of the weather conditions, the temperature, the humidity and the wind speed
+
+//the user will also need to see a five day forecast
+
+//the user should be able to find cities they have previously viewed (using local storage) and this will present them with current and future conditions 
