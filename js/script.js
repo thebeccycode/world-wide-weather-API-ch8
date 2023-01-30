18f71eac41f3efe3aff1caf5b0d0587131b8a806
Rebecca Sellers <rlsellers@hotmail.co.uk>
2023-01-30 09:48:32 +0000

    small changes and notes

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const apiKey = "3426654f99a0dfa7522e4aab3cb64af2";
 
 //event listener for form submission
 $("#searchbox").on("submit", function (event) {
-
+   
 
 });
 
@@ -18,14 +18,9 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=Leeds&limit=5&appid=342665
     .then(response => response.json())
     .then(data => {
         
-        let list = data.list;
-        //console.log(list)
-        console.log(list[0]);
-        //console.log(list[7]);
-//how to turn api object global variables into local variables on vsc
+        let cityList = data.list;
+        console.log("city list:", cityList)
+        //console.log(list[0]);
+        //how to log the list
 
     });
-
-let userCity = " ";
-//need to find a way to link this to query URL so that the users entry is searched when the fetch is returned
-
