e31aafb2c88d8c29463307d2deb4ca21e64ef35b
Rebecca Sellers <rlsellers@hotmail.co.uk>
2023-01-30 10:20:07 +0000

    minor changes to html, css, and js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,16 @@ $("#searchbox").on("submit", function (event) {
 
 });
 
+//add search history to html
+function userSearchHistory(){
+    let button = document.createElement("button")
+    button.addEventListener("click", (event) =>{
+        event.preventDefault()
+        console.log(userSearchHistory);
+    })
+};
+
+//this fetches current weather for location based on co-ordinates 
 fetch("http://api.openweathermap.org/geo/1.0/direct?q=Leeds&limit=5&appid=3426654f99a0dfa7522e4aab3cb64af2")
     .then(response => response.json())
     .then(cityFound => {
@@ -24,3 +34,25 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q=Leeds&limit=5&appid=342665
         //how to log the list
 
     });
+
+    //to do list
+
+    //turn the data in the api into a new object using a function
+
+       // function weatherObject(){
+       //     return{
+       //         location: data.name,
+       //         icon:
+        //        date:,
+        //        temp:,
+        //        humidity:,
+        //        wind:
+        //    }
+        //}
+
+//retrieve data on weather function
+
+//render to current weather card
+
+//render to 5 day forecast cards
+
